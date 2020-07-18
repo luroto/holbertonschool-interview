750a4c526939d505d8129fdc32c24aba913458d9
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-18 02:33:24 +0000

    [Fix] Fixing conditionals

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,13 +9,13 @@ def canUnlockAll(boxes):
     This function determines if all the boxes in a list of lists can be opened
     '''
     keychain = boxes[0].copy()
-    i = 0
+    i = len(boxes)
+
     for key in keychain:
         for llave in boxes[key]:
-            if llave not in keychain:
+            if llave < i and llave not in keychain:
                 keychain.append(llave)
 
-    i = len(boxes)
     for j in range(1, i):
         if j not in keychain:
             return (False)
