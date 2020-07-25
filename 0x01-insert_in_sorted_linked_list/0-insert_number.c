078a7ceceab8131b3a72a476b0680153bf48f60e
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-25 22:24:39 +0000

    [Fix] Checks if head is null at the very beginning of the function

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,6 +9,8 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *aux, *newnode;
 
+	if (head == NULL)
+		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
