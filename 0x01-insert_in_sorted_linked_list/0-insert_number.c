b6f7811109333e1b28dcba7454802026f5a58744
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-07-25 22:19:18 +0000

    [Feature]:Uploading all the files required for this task

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *insert_node - Insert node in a sorted linked list
+ *@head: Entirle linked list
+ *@number: Value to be added
+ *Return: The added node or NULL if it fails
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *aux, *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = number;
+	if ((*head)->n > number)
+	{
+		newnode->next = *head;
+		*head = newnode;
+		return (newnode);
+	}
+	aux = *head;
+	while (aux->next != NULL)
+	{
+		if (aux->next->n > number)
+		{
+			newnode->next = aux->next;
+			aux->next = newnode;
+			return (newnode);
+		}
+		aux = aux->next;
+	}
+	aux->next = newnode;
+	return (newnode);
+}
