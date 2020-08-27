d7b59df4487a192d229ca85ff945530857001e50
Lucia Rodriguez Toloza <774@holbertonschool.com>
2020-08-27 04:16:36 +0000

    [Fix] Reordering for last time

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,78 +1,27 @@
 #include "lists.h"
 /**
- * is_palindrome - Determines if a linked list is a palindrome
- * @head: Pointer to the entire linked list
- * Return: 1 if it is a palindrome, otherwise 0
+ *is_palindrome - Function that checks if a singly linked list is palinrome
+ *@head: Pointer to the entire structure
+ *Return: 1 if is palindrome, otherwise 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = NULL, *aux = NULL, *aux2 = NULL;
-	int size = 0, half = 0, i = 0, pal = 1;
-
-	if (*head == NULL)
-		return (pal);
-	size = size_list(*head);
-	if (size == 1)
-		return (pal);
-	if (size == 2)
-	{
-		if ((*head)->n != (*head)->next->n)
-			return (0);
-		else
-			return (pal);
-	}
-	half = size / 2;
-	aux = *head;
-	while (i < half)
-	{
-		i++;
-		add_node_at_head(&h, aux->n);
-		aux = aux->next;
-	}
-	if (size % 2 != 0)
-		aux = aux->next;
-	aux2 = h;
-	while (aux != NULL)
-	{
-		if (aux->n != aux2->n)
-			pal = 0;
-		aux = aux->next;
-		aux2 = aux2->next;
-	}
-	free_listint(h);
-	return (pal);
+	return (checking_palindrome(head, *head));
 }
 /**
- * size_list - Gets the size of a list
- * @head: pointer to the first node of the structure
- * Return: Number of nodes
+ *checking_palindrome - Uses recursion for moving pointers
+ *@first: First node to be compared
+ *@second: second node to be compared
+ *Return: 1 if it's a palindrome otherwise 0
  */
-int size_list(listint_t *head)
+int checking_palindrome(listint_t **first, listint_t *second)
 {
-	int i = 0;
+	int ou, ah;
 
-	while (head != NULL)
-	{
-		i++;
-		head = head->next;
-	}
-	return (i);
-}
-/**
- * add_node_at_head - add nodes from the original list as stack
- * @head: Structure of the stack
- * @n: Number to be added
- *Return: The created node
- */
-listint_t *add_node_at_head(listint_t **head, int n)
-{
-	listint_t *newnode;
-
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-		return (NULL);
-	newnode->n = n;
-	newnode->next = *head;
-	*head = newnode;
-	return (*head);
+	if (second == NULL)
+		return (1);
+	ou = checking_palindrome(first, second->next);
+	ah = (*first)->n == second->n;
+	(*first) = (*first)->next;
+	return (ou && ah);
 }
