fbe88a0411074332f888e7efe9681d73725be184
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-01 04:33:13 +0000

    [Fix]Fixing conditionals

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -9,63 +9,39 @@
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *preslist = NULL, *nextlist = NULL;
-	size_t size, index, increment;
+	skiplist_t *preslist;
 
-	size = listint_len(list);
-	printf("%ld", size);
-	increment = sqrt(size);
+	if (list == NULL)
+	return (NULL);
 	preslist = list;
-	nextlist = list->express;
 
-	if (!list)
-		return (NULL);
-	while (nextlist->express != NULL)
+	while (preslist != NULL)
 	{
-		printf("Value checked at index [%ld] = [%d]",
-		       preslist->index, preslist->n);
-		if (nextlist->n > value)
+		printf("Value checked at index [%ld] = [%d]\n",
+			preslist->index, preslist->n);
+		if (preslist != NULL && preslist->express != NULL &&
+			preslist->express->n >= value)
 		{
-			printf("Value found between indexes [%ld] and [%ld]",
-				preslist->index, nextlist->index);
-			while (preslist != NULL)
-			{
-				printf("Entro aqui\n");
-				if (preslist->n == value)
-					return (preslist);
-				if (preslist->n > value || preslist->index == size - 1)
-					return (NULL);
-				preslist = preslist->next;
-				index++;
-			}
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				preslist->index, preslist->express->index);
+			break;
+		}
+
+		if (preslist->express->express == NULL && preslist->express->n < value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				preslist->index, preslist->express->index);
+			break;
 		}
 		preslist = preslist->express;
-		nextlist = nextlist->express;
-		index += increment;
+	}
+	while (preslist != NULL)
+	{
+		printf("Value checked at index [%ld] = [%d]\n",
+			preslist->index, preslist->n);
+		if (preslist->n == value)
+			return (preslist);
+		preslist = preslist->next;
 	}
 	return (NULL);
-
-}
-/**
- *listint_len - returns the number of elements in a linked list
- *@h: pointer to the elements to be used as input
- *Return: The number of nodes as a result
-*/
-size_t listint_len(const skiplist_t *h)
-{
-	size_t contanodos = 0;
-
-	if (h == NULL)
-	{
-		return (contanodos);
-	}
-	else
-	{
-		while (h != NULL)
-		{
-			h = h->next;
-			contanodos++;
-		}
-	}
-	return (contanodos);
 }
--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -21,16 +21,16 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-size_t listint_len(const skiplist_t *h);
+
 
 #endif /* _SEARCH_H_ */
