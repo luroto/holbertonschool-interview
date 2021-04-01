adaaf66baf63965b0f6196da52e5063dae9bd421
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-01 03:40:11 +0000

    [Feature]Uploading files

---
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -0,0 +1,58 @@
+#include "search.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *preslist = NULL, *nextlist = NULL;
+	size_t size, index, increment;
+
+	size = listint_len(list);
+	printf("%ld", size);
+	increment = sqrt(size);
+	preslist = list;
+	nextlist = list->express;
+
+	if (!list)
+		return (NULL);
+	while (nextlist->express != NULL)
+	{
+		printf("Value checked at index [%ld] = [%d]", preslist->index, preslist->n);
+		if (nextlist->n > value)
+		{
+			printf("Value found between indexes [%ld] and [%ld]", preslist->index, nextlist->index);
+			while (preslist != NULL)
+			{
+				printf("Entro aqui\n");
+				if (preslist->n == value)
+					return(preslist);
+				if (preslist->n > value || preslist->index == size - 1)
+					return (NULL);
+				preslist = preslist->next;
+				index++; 
+			}
+		}
+		preslist = preslist->express;
+		nextlist = nextlist->express;
+		index += increment;
+	}
+	return (NULL);
+
+}
+
+size_t listint_len(const skiplist_t *h)
+{
+	size_t contanodos = 0;
+
+	if (h == NULL)
+	{
+		return (contanodos);
+	}
+	else
+	{
+		while (h != NULL)
+		{
+			h = h->next;
+			contanodos++;
+		}
+	}
+	return (contanodos);
+}
