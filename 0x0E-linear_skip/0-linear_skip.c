17b8dbd5fe74ebb340b84fc0bb2961c4c5497ed2
Lucia Rodriguez Toloza <774@holbertonschool.com>
2021-04-01 03:44:23 +0000

    [Feature] Uploading just files

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,4 +1,11 @@
 #include "search.h"
+/**
+*linear_skip - function to perform a search in a linear skip
+*@list: Head of the list
+*@value: Integer to be searched
+*Return: NULL if list is empty or value is not found,
+*otherwise, pointer to the first occurrence of the value
+*/
 
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
@@ -15,19 +22,21 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		return (NULL);
 	while (nextlist->express != NULL)
 	{
-		printf("Value checked at index [%ld] = [%d]", preslist->index, preslist->n);
+		printf("Value checked at index [%ld] = [%d]",
+		       preslist->index, preslist->n);
 		if (nextlist->n > value)
 		{
-			printf("Value found between indexes [%ld] and [%ld]", preslist->index, nextlist->index);
+			printf("Value found between indexes [%ld] and [%ld]",
+				preslist->index, nextlist->index);
 			while (preslist != NULL)
 			{
 				printf("Entro aqui\n");
 				if (preslist->n == value)
-					return(preslist);
+					return (preslist);
 				if (preslist->n > value || preslist->index == size - 1)
 					return (NULL);
 				preslist = preslist->next;
-				index++; 
+				index++;
 			}
 		}
 		preslist = preslist->express;
@@ -37,7 +46,11 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	return (NULL);
 
 }
-
+/**
+ *listint_len - returns the number of elements in a linked list
+ *@h: pointer to the elements to be used as input
+ *Return: The number of nodes as a result
+*/
 size_t listint_len(const skiplist_t *h)
 {
 	size_t contanodos = 0;
